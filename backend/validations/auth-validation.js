49d7e5797f8dbc63b9a98da95f808ee67f1f1e91
Dmitry Verteyko <verteyko1990@gmail.com>
2022-07-27 14:45:43 +0300

    added login/register/getMe

--- backend/validations/auth-validation.js
+++ backend/validations/auth-validation.js
@@ -1,8 +1,13 @@
 import { body } from 'express-validator';
 
+export const loginValidation = [
+    body('email', 'Неверный формат почты').isEmail(),
+    body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
+];
+
 export const registerValidation = [
     body('email', 'Неверный формат почты').isEmail(),
-    body('password', 'Недостаточно защищенный пароль нужен минимум 5').isLength({min: 5}),
-    body('fullName', 'Минимальное количество символов 3').isLength({min: 3}),
-    body('avatarUrl', 'Неверно указан URL аватарки').optional().isURL(),
+    body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
+    body('fullName', 'Укажите имя').isLength({ min: 3 }),
+    body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
 ];
