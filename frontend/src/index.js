31da2a2a8fa2584bc10b9bb2ca35252e797e0240
Dmitry Verteyko <verteyko1990@gmail.com>
2022-07-27 21:13:15 +0300

    added markup for frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from 'react-router-dom';
 import CssBaseline from "@mui/material/CssBaseline";
 
 import "./index.scss";
@@ -13,7 +14,9 @@ root.render(
   <React.StrictMode>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
