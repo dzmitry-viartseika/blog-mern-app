ff186e9bb19a33743157b26438784471ed747feb
Dmitry Verteyko <verteyko1990@gmail.com>
2022-07-28 14:29:14 +0300

    added posts and tags logic

--- frontend/src/redux/slices/auth-slice.js
+++ frontend/src/redux/slices/auth-slice.js
@@ -1,64 +1,76 @@
-import { createSlice, createAsyncThunk  } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from '../../api/api.js';
 
-export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-    const { data } = await axios.get('/posts');
-    console.log('data', data);
+export const fetchAuth = createAsyncThunk('auth/fetchAuth', async (params) => {
+    const { data } = await axios.post('/auth/login', params);
     return data;
 });
 
-export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
-    const { data } = await axios.get('/tags');
-    console.log('data', data);
+export const fetchRegister = createAsyncThunk('auth/fetchRegister', async (params) => {
+    const { data } = await axios.post('/auth/register', params);
     return data;
 });
 
-
-
+export const fetchAuthMe = createAsyncThunk('auth/fetchAuthMe', async () => {
+    const { data } = await axios.get('/auth/me');
+    return data;
+});
 
 const initialState = {
-    posts: {
-        items: [],
-        status: 'loading',
-    },
-    tags: {
-        items: [],
-        status: 'loading',
-    },
+    user: null,
+    status: 'loading',
 };
 
-const postsSlice = createSlice({
-    name: 'posts',
+const authSlice = createSlice({
+    name: 'auth',
     initialState,
-    reducers: {},
-    extraReducers: {
-        // GET POSTS
-        [fetchPosts.pending]: (state) => {
-            state.posts.items = [];
-            state.posts.status = 'loading';
-        },
-        [fetchPosts.fulfilled]: (state, action) => {
-            state.posts.items = action.payload;
-            state.posts.status = 'loaded';
-        },
-        [fetchPosts.rejected]: (state) => {
-            state.posts.items = [];
-            state.posts.status = 'error';
-        },
-        // GET TAGS
-        [fetchTags.pending]: (state) => {
-            state.tags.items = [];
-            state.tags.status = 'loading';
-        },
-        [fetchTags.fulfilled]: (state, action) => {
-            state.tags.items = action.payload;
-            state.tags.status = 'loaded';
-        },
-        [fetchTags.rejected]: (state) => {
-            state.tags.items = [];
-            state.tags.status = 'error';
+    reducers: {
+        logout: (state) => {
+            state.user = null;
         },
     },
-})
+    extraReducers: {
+        [fetchAuth.pending]: (state) => {
+            state.status = 'loading';
+            state.user = null;
+        },
+        [fetchAuth.fulfilled]: (state, action) => {
+            state.status = 'loaded';
+            state.user = action.payload;
+        },
+        [fetchAuth.rejected]: (state) => {
+            state.status = 'error';
+            state.user = null;
+        },
+        [fetchAuthMe.pending]: (state) => {
+            state.status = 'loading';
+            state.user = null;
+        },
+        [fetchAuthMe.fulfilled]: (state, action) => {
+            state.status = 'loaded';
+            state.user = action.payload;
+        },
+        [fetchAuthMe.rejected]: (state) => {
+            state.status = 'error';
+            state.user = null;
+        },
+        [fetchRegister.pending]: (state) => {
+            state.status = 'loading';
+            state.user = null;
+        },
+        [fetchRegister.fulfilled]: (state, action) => {
+            state.status = 'loaded';
+            state.user = action.payload;
+        },
+        [fetchRegister.rejected]: (state) => {
+            state.status = 'error';
+            state.user = null;
+        },
+    },
+});
 
-export const postsReducer = postsSlice.reducer;
+export const selectIsAuth = (state) => Boolean(state.auth);
+
+export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { postsReducer } from './slices/posts-slice.js';
+import { authReducer } from './slices/auth-slice.js';
 
 const store = configureStore({
     reducer: {
         posts: postsReducer,
+        user: authReducer,
     }
 });
 
